029053a2ecf1c27a2d979b65e37437587312cc9b
Torick Davis <torickjdavis@gmail.com>
2017-04-21 23:32:13 +0000

    More Dynamic Page

--- js/main.js
+++ js/main.js
@@ -1,31 +1,39 @@
 /*global $*/
 
 $(document).ready(function() {
-    window.nav = {
-        toggle: function() {
-            var navToggler = $("#navToggler");
-            var navIcon = navToggler.children().first();
-            if(this.isOpen) {
-                $("main").css({"margin-right":"0"});
-                $("aside").css({"width":"0"});
-                navIcon.removeClass('fa-times');
-                navIcon.addClass('fa-bars');
-                this.isOpen = false;
-            } else {
-                $("main").css({"margin-right":"25%"});
-                $("aside").css({"width":"25%"});
-                navIcon.removeClass('fa-bars');
-                navIcon.addClass('fa-times');
-                this.isOpen = true;
-            }
-            
-        },
-        isOpen: false
-    }
-    $('#navToggler').click(nav.toggle);
+    dynamicBits();
+});
+
+function dynamicBits() {
+    generateNav();
     $('.external').each(function() {
         $(this).before('<i class="fa fa-external-link" aria-hidden="true"></i>');
         $(this).attr('target','_blank');
     });
     $('footer').html('&copy; 2017 | AF "Herald" Team');
-});
+}
+
+function generateNav() {
+        var nav = {
+            toggle: function() {
+                var navToggler = $("#navToggler");
+                var navIcon = navToggler.children().first();
+                if(this.isOpen) {
+                    $("main").css({"margin-right":"0"});
+                    $("aside").css({"width":"0"});
+                    navIcon.removeClass('fa-times');
+                    navIcon.addClass('fa-bars');
+                    this.isOpen = false;
+                } else {
+                    $("main").css({"margin-right":"25%"});
+                    $("aside").css({"width":"25%"});
+                    navIcon.removeClass('fa-bars');
+                    navIcon.addClass('fa-times');
+                    this.isOpen = true;
+                }
+                
+            },
+            isOpen: false
+    }
+    $('#navToggler').click(nav.toggle);
+}
