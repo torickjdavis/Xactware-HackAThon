ce7c66309f0b1f02c80c439dfb3e2d93cbee25ad
Torick Davis <torickjdavis@gmail.com>
2017-04-21 22:03:50 +0000

    Update to design

--- js/main.js
+++ js/main.js
@@ -2,19 +2,29 @@
 
 $(document).ready(function() {
     window.nav = {
-        open: function() {
-            $("main").css({"margin-right":"25%"});
-            $("aside").css({"width":"25%"});
-            $("header")
-            console.log("Nav:Open");
+        toggle: function() {
+            var navToggler = $("#navToggler");
+            var navIcon = navToggler.children().first();
+            if(this.isOpen) {
+                $("main").css({"margin-right":"0"});
+                $("aside").css({"width":"0"});
+                navIcon.removeClass('fa-times');
+                navIcon.addClass('fa-bars');
+                this.isOpen = false;
+            } else {
+                $("main").css({"margin-right":"25%"});
+                $("aside").css({"width":"25%"});
+                navIcon.removeClass('fa-bars');
+                navIcon.addClass('fa-times');
+                this.isOpen = true;
+            }
+            
         },
-        close: function() {
-            $("main").css({"margin-right":"0"});
-            $("aside").css({"width":"0"});
-            console.log("Nav:Close");
-        }
+        isOpen: false
     }
-    
-    $('#navClose').click(nav.close);
-    $('#navOpen').click(nav.open);
+    $('#navToggler').click(nav.toggle);
+    $('.external').each(function() {
+        $(this).before('<i class="fa fa-external-link" aria-hidden="true"></i>');
+        $(this).attr('target','_blank');
+    });
 });
