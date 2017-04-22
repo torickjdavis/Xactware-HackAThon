915971d230cb97c69e1631315edfec9afcb926aa
Torick Davis <torickjdavis@gmail.com>
2017-04-22 04:13:03 +0000

    Mostert revamp 2

---
+++ script.js
@@ -0,0 +1,31 @@
+/*global $*/
+$(document).ready(function() {
+    var splashHeight = $('.splash').outerHeight();
+    $.ajax({url: "./pages/home.html", success: function(result){
+            $(".well").html(result);}});
+    
+    $('.fa-chevron-down').click(function(){//auto scroll with chevron
+        $('body').animate({scrollTop: splashHeight}, 2000)//.scrollTop(splashHeight);
+    });
+    
+    $(window).scroll(function(){
+        var smallHeader = $('.smallHeader');
+        var chevron = $('.fa-chevron-down');
+        // console.log($('body').scrollTop());
+        if($('body').scrollTop() > (splashHeight / 3 * 2)) {
+            smallHeader.attr('hidden', false);
+            chevron.hide();
+        } else {
+            smallHeader.attr('hidden', true);
+            chevron.show();
+        }
+    });
+    
+    $('nav li').click(function() {
+        var url = $(this).text().toLowerCase();
+        $.ajax({url: "./pages/" + url + ".html", success: function(result){
+            $(".well").html(result);
+        }
+        });
+    });
+});
