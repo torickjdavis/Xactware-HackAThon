2d9250c228ff69a6504a480fe97ced35d26ea72d
Gideon Roberts <giddavidr@gmail.com>
2017-04-22 03:52:02 +0000

    test

--- js/main.js
+++ js/main.js
@@ -37,3 +37,27 @@ function generateNav() {
     }
     $('#navToggler').click(nav.toggle);
 }
+ function calculateAltitude () {
+        var elev = prompt("Please enter your elevation.", "0");
+        var time = prompt("please enter your time.", "0");
+        var final;
+        if (elev < 3124 || elev === "null") {
+        alert("Your speed wouldn't change with this altitude!");
+    }
+        else if (elev < 4009 && elev > 3124) {
+        final=time - 12.29;
+        alert("Your time would be "+final+" seconds!");
+    }
+        else if (elev < 6001 && elev > 4009) {
+            final=time - 17.36;
+        alert("Your time would be "+final+" seconds less!");
+    }   
+        else if (elev < 7703 && elev > 6001) {
+        final=time - 32.76;
+        alert("Your time would be "+final+" seconds less!");
+    }
+        else {
+        alert("Invalid Input!");    
+    }
+}
+document.getElementById('altitudecalc').innerHTML.onclick=calculateAltitude();
